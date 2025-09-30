3e35c11a639a238fc2fcf6534d96bc433180d885
baskaraid <83570860+baskaraid@users.noreply.github.com>
2025-09-30 21:31:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",         # sesuaikan user mysql
-        password="",         # sesuaikan password mysql
-        database="study_tracer"
+        host="localhost",                # tetap localhost karena MySQL di hosting yang sama
+        user="appbaswe_studytracer",     # user database
+        password="appbaswe_studytracer",# password user DB yang kamu buat di cPanel
+        database="appbaswe_studytracer"  # nama database
     )
